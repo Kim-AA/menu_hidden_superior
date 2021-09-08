8369a762536febb8d2f33b500ba5e45c6c7acbe2
timfel20 <timmyfelicia@gmail.com>
2021-09-08 19:06:31 +0200

    okrrr

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let menu_top = document.querySelector('.menu_top');
 let menu_bottom = document.querySelector('.menu_bottom');
 
-let lastScrollY = window.scrollY
+let anything = window.scrollY
 window.addEventListener('scroll', function(){
-    if(lastScrollY < window.scrollY){
+    if(anything < window.scrollY){
         menu_bottom.classList.add('hide_down');
         menu_bottom.classList.remove('hide_up')
     }
-    else{menu_bottom.classList.remove('hide_down')
-         menu_top.classList.add('hide_up');
+    else{ menu_top.classList.add('hide_up');
+          menu_bottom.classList.remove('hide_down')
     }
 });
 
