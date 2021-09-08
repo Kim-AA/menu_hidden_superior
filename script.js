5ccc26f749b957412f4205f0178fecfa4b242d82
Kim--AA <aliaga.aguirre.kimberly@gmail.com>
2021-09-08 12:05:36 +0200

    js

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ let menu_top = document.querySelector('.menu_top');
 let menu_bottom = document.querySelector('.menu_bottom');
 
 document.addEventListener('scroll', function(){
-    menu_top.classList.add('transform')
+    menu_top.classList.add('transform_down')
 });
 
 document.addEventListener('scroll', function(){
-    menu_bottom.classList.add('hide')
+    menu_bottom.classList.add('hide_down')
 });
+
+
+document.addEventListener('scroll', function(){
+    menu_top.classList.add('transform_up')
+});
+
+document.addEventListener('scroll', function(){
+    menu_bottom.classList.add('hide_up')
+});
+
+
